b189352b0dce624308c4b10676f80c3622262320
Tanmay Banjari <ma23btech11025@iith.ac.in>
2026-08-27 17:05:22 +0530

    feat: add TCP server startup

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
-#include <bits/stdc++.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-using namespace std;
+#include "network/tcp_server.h"
+#include "core/logger.h"
 
 int main() {
-    cout << "Message Gateway" << endl;
+    TCPServer::install_signal_handlers();
+    TCPServer server(8080);
+
+    if (!server.start()) {
+        Logger::error("Failed to start server");
+        return 1;
+    }
+
+    server.accept_loop();
+    Logger::info("Server shut down cleanly");
     return 0;
 }
